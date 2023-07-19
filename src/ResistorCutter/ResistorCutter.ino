10ae400ce3f173ea875fff5e9e9ca5827096be1b
radiantBear <85893453+radiantBear@users.noreply.github.com>
2023-07-19 09:47:43 -0700

    Add comments to ResistorCutter.ino

--- src/ResistorCutter/ResistorCutter.ino
+++ src/ResistorCutter/ResistorCutter.ino
@@ -8,12 +8,16 @@
  * Last updated: 07/18/2023
  */
 
+/**
+ * @todo change Interface.h to define macros for RUNNING, PAUSED, NOT_RUNNING?
+ */
+
 #include "pins.h"       // List of all pin connections
 #include "Interface.h"  // For I/O using the LCD and joystick
 
 Interface interface(CLK_PIN, DIN_PIN, DC_PIN, CE_PIN, RST_PIN, VRx_PIN, VRy_PIN, SW_PIN, SAFE_PIN);
 
-void printProgress(bool state);
+void handleStateChange(int state);
 void checkPause(void);
 
 void setup() {
@@ -22,7 +26,7 @@ void setup() {
     Serial.println("\n\nResistor cutter, compiled " __DATE__ " " __TIME__ " by bairdn");
 
     interface.setup();
-    interface.setButtonListener(printProgress);
+    interface.setButtonListener(handleStateChange);
 }
 
 void loop() {
@@ -31,6 +35,9 @@ void loop() {
     checkPause();
 }
 
+/**
+ * @brief Checks if the user passed a pause/resume command through the Serial interface
+ */
 void checkPause(void) {
     if(Serial.available()) {
         String input = Serial.readStringUntil('\n');
@@ -43,17 +50,34 @@ void checkPause(void) {
     }
 }
 
-void printProgress(bool state) {
-    Serial.printf("Button pressed! Machine is %s.", interface.getRunning() ? "on" : "off");
-    if(state) Serial.printf(" Cutting groups of %d resistors for %d kits.\n", interface.getResistorsPerKit(), interface.getKits());
-    else Serial.println();
+/**
+ * @brief Handles UI button press (start/stop the machine)
+ *     - Prints diagnostic info based on running state
+ *     - Gradually fills the progress bar when the resistor cutter is started
+ *
+ * @param state The current running state: 0 for stopped, 1 for running, 2 for paused
+ */
+void handleStateChange(int state) {
+    Serial.printf("Button pressed! Machine is %s.", interface.getRunningStatus() ? "on" : "off");
+    if(state == 2) {
+        // Shouldn't be possible
+        Serial.println("\nERR: Somehow, handleStateChange() was called while paused");
+    } else if(state == 1) {
+        Serial.printf(" Cutting groups of %d resistors for %d kits.\n", interface.getResistorsPerKit(), interface.getKits());
+    } else {
+        Serial.println();
+        return;
+    }
 
     for(int i = 0; i <= 100; i++) {
         interface.update(i);// call function to update completion bar
 
-        if(interface.getRunning() == 2) i--;
+        if(interface.getRunningStatus() == 2) i--;
         checkPause();
 
         delay(50);
     }
+
+    // Have to tell Interface.h that the machine is no longer running
+    interface.doneRunning();
 }
