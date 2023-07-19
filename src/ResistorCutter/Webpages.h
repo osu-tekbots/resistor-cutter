76d5a89a78327d44249d53210ad7be351b155650
radiantBear <85893453+radiantBear@users.noreply.github.com>
2023-07-18 17:21:06 -0700

    Update Webpages.h
    Add string replacements to fill in the main HTML placeholders with current info

--- src/ResistorCutter/Webpages.h
+++ src/ResistorCutter/Webpages.h
@@ -4,45 +4,74 @@
  * Nathaniel Baird
  * bairdn@oregonstate.edu
  *
- * Started: 07/13/2023
- * Last updated: 07/13/2023
+ * Started:      07/13/2023
+ * Last updated: 07/18/2023
  */
 
 class Webpages {
     private:
         const char* captiveHTML = R"=====(
-                <!DOCTYPE html> 
-                <html>
-                    <head>
+            <!DOCTYPE html> 
+            <html>
+                <head>
                     <title>ESP32 Captive Portal</title>
                     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                    <meta http-equiv="refresh" content="0; url=http://www.tekbots.com/?redirect=true">
-                    </head>
-                    <body>
-                    <h1><a href="http://www.tekbots.com/?redirect=true">Click me to view resistor cutter status</a></h1>
-                    </body>
-                </html>
+                    <meta http-equiv="refresh" content="0; url=http://www.neverssl.com/?redirect=true">
+                </head>
+                <body>
+                    <h1><a href="http://www.neverssl.com/?redirect=true">Click me to view resistor cutter status</a></h1>
+                </body>
+            </html>
             )=====";
         const char* successHTML = R"=====(
-                <!DOCTYPE html>
-                <html>
-                    <head>
-                        <title>Success</title>
-                        <meta http-equiv="refresh" content="0; url=http://www.tekbots.com/?redirect=true">
-                    </head>
-                    <body>
-                        Success
-                    </body>
-                </html>
+            <!DOCTYPE html>
+            <html>
+                <head>
+                    <title>Success</title>
+                    <meta http-equiv="refresh" content="0; url=http://www.neverssl.com/?redirect=true">
+                </head>
+                <body>
+                    Success
+                </body>
+            </html>
             )=====";
         const char* mainHTML = R"=====(
-                <!DOCTYPE html> 
-                <html>
-                    <head>
+            <!DOCTYPE html> 
+            <html>
+                <head>
                     <title>ESP32 Captive Portal</title>
                     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                    <meta http-equiv="refresh" content="5" url="http://www.tekbots.com/?redirect=true">
+                    <meta http-equiv="refresh" content="1" url="http://www.neverssl.com/?redirect=true">
                     <style>
+                        body {
+                            display: flex;
+                            align-items: center;
+                            flex-direction: column;
+                        }
+
+                        .container {
+                            display: flex;
+                            justify-content: center;
+                            align-items: center;
+                            margin: 5px;
+                        }
+
+                        div > div {
+                            width: 200px;
+                            height: 125px;
+
+                            display: flex;
+                            flex-direction: column;
+                            justify-content: center;
+                            align-items: center;
+                            text-align: center;
+                        }
+
+                        #status > div {
+                            border: 2px solid black;
+                            border-radius: 8px;
+                        }
+
                         .cutting {
                             background: #98ff98;
                         }
@@ -51,31 +80,37 @@ class Webpages {
                             background: #ff9898;
                         }
 
-                        #container {
-                            display: flex;
-                            justify-content: center;
-                            align-items: center;
+                        #data {
+                            border: 2px solid black;
+                            border-radius: 8px;
+                            max-width: 400px;
                         }
 
-                        div > div {
-                            width: 200px;
-                            height: 125px;
-
-                            display: flex;
-                            justify-content: center;
-                            align-items: center;
-                            text-align: center;
+                        #data h2 {
+                            margin-bottom: -10px;
+                            color: darkgrey;
                         }
                     </style>
-                    </head>
-                    <body>
-                    <h1 style="text-align: center; width: 100%;">Resistor Cutter Status</h1>
-                    <div id="container">
-                        <div id="status" class="cutting"><p>Cutting</p></div>
-                        <div id="status2" class="notCutting"><p>Not Cutting</p></div>
+                </head>
+                <body>
+                    <h1>Resistor Cutter Status</h1>
+
+                    <div id="data" class="container">
+                        <div id="rPerKit" style="border-right: 1px solid black;">
+                            <h2>Resistors Per Kit</h2>
+                            <h1>{{rPerKit}}</h1>
+                        </div>
+                        <div id="kits">
+                            <h2>Kits</h2>
+                            <h1>{{kits}}</h1>
+                        </div>
                     </div>
-                    </body>
-                </html>
+                    
+                    <div id="status" class="container">
+                        <div class="{{cuttingClass}}"><h1>{{cuttingText}}</h1></div>
+                    </div>
+                </body>
+            </html>
             )=====";
 
         int rPerKit, kits, percent;
@@ -95,11 +130,11 @@ class Webpages {
         void setKits(int kits) {
             this->kits = kits;
         }
-        void setPercent(int percent) {
-            this->percent = percent;
-        }
         void setRunning(bool running) {
             this->running = running;
+        }
+        void setPercent(int percent) {
+            this->percent = percent;
         }
 
         const char* getCaptiveHTML() {
@@ -111,6 +146,23 @@ class Webpages {
         }
 
         const char* getMainHTML() {
-            return mainHTML;
+            static std::string htmlCopy;
+            size_t index = 0;
+
+            htmlCopy = mainHTML;
+
+            index = htmlCopy.find("{{rPerKit}}");
+            if(index != std::string::npos) htmlCopy.replace(index, strlen("{{rPerKit}}"), String(rPerKit).c_str());
+
+            index = htmlCopy.find("{{kits}}");
+            if(index != std::string::npos) htmlCopy.replace(index, strlen("{{kits}}"), String(kits).c_str());
+            
+            index = htmlCopy.find("{{cuttingClass}}");
+            if(index != std::string::npos) htmlCopy.replace(index, strlen("{{cuttingClass}}"), running ? "cutting" : "notCutting");
+            
+            index = htmlCopy.find("{{cuttingText}}");
+            if(index != std::string::npos) htmlCopy.replace(index, strlen("{{cuttingText}}"), running ? "Cutting" : "Not Cutting");
+
+            return htmlCopy.c_str();
         }
 };
