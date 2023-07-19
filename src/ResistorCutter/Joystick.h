7c28e19a92f1cb2544046f3fcd81a429a33c76b1
radiantBear <85893453+radiantBear@users.noreply.github.com>
2023-07-19 09:47:05 -0700

    Add comments to Joystick.h

--- src/ResistorCutter/Joystick.h
+++ src/ResistorCutter/Joystick.h
@@ -13,6 +13,11 @@ class Joystick {
         int8_t vrX, vrY, sw;
 
     public:
+        /**
+         * @param vrX   The ESP32 pin connected to the joystick's VRx pin
+         * @param vrY   The ESP32 pin connected to the joystick's VRy pin
+         * @param sw    The ESP32 pin connected to the joystick's SW pin
+         */
         Joystick(int8_t vrX, int8_t vrY, int8_t sw) {
             this->vrX = vrX;
             this->vrY = vrY;
@@ -21,34 +26,61 @@ class Joystick {
             pinMode(sw, INPUT_PULLUP);
         }
         
+        /**
+         * @return Whether the joystick switch is pressed down
+         */
         bool getSwitch() {
             return !digitalRead(sw);
         }
 
+        /**
+         * @return Whether the joystick is angled left
+         */
         bool getLeft() {
             return analogRead(vrX) < 0 + 500;
         }
 
+
+        /**
+         * @return Whether the joystick is angled right
+         */
         bool getRight() {
             return analogRead(vrX) > 4095 - 500;
         }
 
+
+        /**
+         * @return Whether the joystick is angled up
+         */
         bool getUp() {
             return analogRead(vrY) < 0 + 500;
         }
 
+
+        /**
+         * @return Whether the joystick is angled down
+         */
         bool getDown() {
             return analogRead(vrY) > 4095 - 500;
         }
 
+        /**
+         * @return Whether the joystick is angled any direction
+         */
         bool getUncentered() {
             return getUp() || getDown() || getLeft() || getRight();
         }
 
+        /**
+         * @return Whether the joystick is tilted vertically (up/down)
+         */
         bool getVertical() {
             return getUp() || getDown();
         }
 
+        /**
+         * @return Whether the joystick is tilted horizontally (left/right)
+         */
         bool getHorizontal() {
             return getLeft() || getRight();
         }
