046f5eb73ca22d8d169d918fd4f702184838da1a
radiantBear <85893453+radiantBear@users.noreply.github.com>
2023-07-18 17:06:10 -0700

    Update Interface.h

--- src/ResistorCutter/Interface.h
+++ src/ResistorCutter/Interface.h
@@ -23,66 +23,6 @@ private:
     bool          running, debounced, switchPressed;
     void (*callbackFn)(bool);
 
-    void handleJoystick() {
-        if(joystick.getVertical() && (debounced || millis() - lastUpdate >= 500)) {
-            lastUpdate = millis();
-            debounced = false;
-
-            if (joystick.getUp()) {
-                currentSelection = (currentSelection + 2) % 3;
-            } else if (joystick.getDown()) {
-                ++currentSelection %= 3;
-            }
-
-            display.updateAll(currentSelection, rPerKit, kits, percent, running);
-            localHost.updatePageInfo(rPerKit, kits, running);
-        } else if(joystick.getHorizontal() && (debounced || millis() - lastUpdate >= 250) && !joystick.getVertical()) {
-            lastUpdate = millis();
-            debounced = false;
-
-            if (currentSelection == 0) {
-                // Max of 10 per kit
-                if (joystick.getLeft()) {
-                    rPerKit = (rPerKit + 8) % 10 + 1;
-                } else {
-                    rPerKit = rPerKit % 10 + 1;
-                }
-            } else if (currentSelection == 1) {
-                // Max of 50 kits
-                if (joystick.getLeft()) {
-                    kits = (kits + 48) % 50 + 1;
-                } else {
-                    kits = kits % 50 + 1;
-                }
-            }
-
-            display.updateAll(currentSelection, rPerKit, kits, percent, running);
-            localHost.updatePageInfo(rPerKit, kits, running);
-        }
-    }
-
-    void handleSwitch() {
-        if(!switchPressed && currentSelection == 2) {
-            switchPressed = true;
-        
-            running = !running;
-
-            display.updateAll(currentSelection, rPerKit, kits, percent, running);
-            localHost.updatePageInfo(rPerKit, kits, running);
-
-            callbackFn(running);
-
-            if(running) {
-                if(running != 2) running = false;
-                else prevRunning = false;
-
-                percent = 0;
-                display.updateAll(currentSelection, rPerKit, kits, percent, running);
-                localHost.updatePageInfo(rPerKit, kits, running);
-            }
-        }
-    }
-
 public:
     Interface(int8_t dispClk, int8_t dispDin, int8_t dispDc, int8_t dispCe, int8_t dispRst,
         int8_t jstkX, int8_t jstkY, int8_t jstkSw)
@@ -103,7 +43,35 @@ public:
     void update(int percent = 0) {
         if(!running) { 
             if(joystick.getUncentered()) {
-                handleJoystick();
+                if(joystick.getVertical() && (debounced || millis() - lastUpdate >= 500)) {	
+                    lastUpdate = millis();	
+                    debounced = false;	
+                    if (joystick.getUp()) {	
+                        currentSelection = (currentSelection + 2) % 3;	
+                    } else if (joystick.getDown()) {	
+                        ++currentSelection %= 3;	
+                    }	
+                    display.updateAll(currentSelection, rPerKit, kits, percent, running);	
+                } else if(joystick.getHorizontal() && (debounced || millis() - lastUpdate >= 250) && !joystick.getVertical()) {	
+                    lastUpdate = millis();	
+                    debounced = false;	
+                    if (currentSelection == 0) {	
+                        // Max of 10 per kit	
+                        if (joystick.getLeft()) {	
+                            rPerKit = (rPerKit + 8) % 10 + 1;	
+                        } else {	
+                            rPerKit = rPerKit % 10 + 1;	
+                        }	
+                    } else if (currentSelection == 1) {	
+                        // Max of 50 kits	
+                        if (joystick.getLeft()) {	
+                            kits = (kits + 48) % 50 + 1;	
+                        } else {	
+                            kits = kits % 50 + 1;	
+                        }	
+                    }	
+                    display.updateAll(currentSelection, rPerKit, kits, percent, running);	
+                }
             } else {
                 debounced = true;
             }
@@ -113,7 +81,18 @@ public:
         }
 
         if(joystick.getSwitch()) {
-            handleSwitch();
+            if(!switchPressed && currentSelection == 2) {	
+                switchPressed = true;	
+            	
+                running = !running;	
+                display.updateAll(currentSelection, rPerKit, kits, percent, running);	
+                callbackFn(running);	
+                if(running) {	
+                    running = false;	
+                    percent = 0;	
+                    display.updateAll(currentSelection, rPerKit, kits, percent, running);	
+                }	
+            }
         } else {
             switchPressed = false;
         }
